ae1bc674886eab08a57c9d7b2b77d687ec655738
justjj92 <justin.jordan92@gmail.com>
2022-10-20 20:22:30 -0700

    Initial Commit

---
+++ Sample_Project/Sample_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sample_ProjectApp.swift
+//  Sample_Project
+//
+//  Created by J J on 10/20/22.
+//
+
+import SwiftUI
+
+@main
+struct Sample_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
